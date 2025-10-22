5d018bb649b9354c0919b01af6f44127e6b2b248
ƬN丅 ｌlᴠɆ (ᴼᶠᶠⁱᶜⁱᵃˡ) <145314654+hoycmd@users.noreply.github.com>
2025-10-22 10:11:01 +0700

    Update default_timer.js

--- client/room/default_timer.js
+++ client/room/default_timer.js
@@ -8,8 +8,8 @@ const length = Room.GameMode.Parameters.GetString('GameLength');
 export function GameModeMtchTime() {
     switch (length) {
         case 'Min30MatchTime': return 1801;
-        case 'Length_M': return 3601; 
-        case 'Length_L': return 1201;
+        case 'Hour1MatchTime': return 3601; 
+        case 'Min20MatchTime': return 1201;
     }   
     return 1201;
 }
