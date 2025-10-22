8a565242122e1befd249c16fea626a078dbc4845
ƬN丅 ｌlᴠɆ (ᴼᶠᶠⁱᶜⁱᵃˡ) <145314654+hoycmd@users.noreply.github.com>
2025-10-22 12:16:04 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -64,10 +64,10 @@ Room.Teams.OnRequestJoinTeam.Add(function (p,t) {
  t.Add(p); 
 p.Properties.Get('RoomID').Value = p.IdInRoom;
 if (Room.GameMode.Parameters.GetBool('Waiting2Player')) {
-if (Room.Players.All.length <= 1) {
+if (Room.Players.All.length == 1) {
  Room.Ui.GetContext().Hint.Value = '<b>\nДля начала, необходимо кол-во игроков: 2</b>';
 } 
-if (Room.Players.All.length >= 1) {
+if (Room.Players.All.length == 2) {
   SetWaitingMode();
   }
 }
